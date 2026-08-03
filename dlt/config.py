e53ecf13a62a165fc8c473432878bbc7a8482cef
Rodrigo <rodrigomarinho.eq@gmail.com>
2026-08-03 13:44:23 -0300

    include reader

---
+++ dlt/config.py
@@ -0,0 +1,20 @@
+from pyspark.sql.streaming import DataStreamReader
+
+INPUT_PATH = "/Volumes/funds-investiments-transactions-current/landing/"
+
+CLOUDFILES_OPTIONS = {
+    "cloudFiles.format": "json",
+    "cloudFiles.includeExistingFiles": "true",
+    "cloudFiles.schemaEvolutionMode": "rescue",
+    "cloudFiles.allowOverwrites": "true",
+    "pathGlobFilter": "*.json",
+    "multiline":"true"
+}
+
+def cloudfiles_reader(reader: DataStreamReader) -> DataStreamReader:
+    reader = reader.fomart("cloudFiles")
+
+    for option, value in CLOUDFILES_OPTIONS.items():
+        reader = reader.option(option,value)
+
+    return reader
