f532b5fb57e74430d92a75c310e86fcbfa35fa0a
Rodrigo <rodrigomarinho.eq@gmail.com>
2026-08-06 10:09:49 -0300

    refactor ddl scripts

--- imperativo/silver/schema_silver.py
+++
@@ -1,8 +0,0 @@
-from common.config import SILVER_TABLE
-from common.spark import spark
-
-SILVER_SCHEMA = ".".join(SILVER_TABLE.split(".")[:2])
-
-
-def create_silver_schema() -> None:
-    spark.sql(f"CREATE SCHEMA IF NOT EXISTS {SILVER_SCHEMA}")
--- imperativo/silver/tables_silver_config.py
+++ imperativo/silver/tables_silver_config.py
@@ -1,6 +1,12 @@
 from common.config import SILVER_REJECTED_TABLE, SILVER_TABLE
 from common.spark import spark
 
+SILVER_SCHEMA = ".".join(SILVER_TABLE.split(".")[:2])
+
+
+def create_silver_schema() -> None:
+    spark.sql(f"CREATE SCHEMA IF NOT EXISTS {SILVER_SCHEMA}")
+
 
 def create_silver_table() -> None:
     spark.sql(f"""
